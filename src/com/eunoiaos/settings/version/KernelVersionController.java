2ae77f07ad57e2d2c7cd3e247a56c52b7f4b00eb
Alex <alex@dirtyunicorns.com>
2018-07-05 16:54:29 -0500

    EunoiaVersion: Change kernel preference behavior
    Before you could only click once to see the full kernel version and if
    you wanted it to go back to the short version, you had to exit the fragment.
    
    Now you can click between the two (short and long).
    
    Change-Id: I9a6d4f361183634bc4a34cdff4a0fe7b00277604

--- src/com/eunoiaos/settings/version/KernelVersionController.java
+++ src/com/eunoiaos/settings/version/KernelVersionController.java
@@ -33,6 +33,7 @@ public class KernelVersionController extends BasePreferenceController {
     private static final String KEY_KERNEL_VERSION = "kernel_version";
     private static final String FILENAME_PROC_VERSION = "/proc/version";
     private static final String LOG_TAG = "KernelVersionController";
+    private boolean fullKernelVersion = false;
 
     public KernelVersionController(Context context, String preferenceKey) {
         super(context, preferenceKey);
@@ -58,7 +59,13 @@ public class KernelVersionController extends BasePreferenceController {
         if (!TextUtils.equals(preference.getKey(), KEY_KERNEL_VERSION)) {
             return false;
         }
-        preference.setSummary(getFullKernelVersion());
+        if(fullKernelVersion) {
+            preference.setSummary(DeviceInfoUtils.getFormattedKernelVersion(mContext));
+            fullKernelVersion = false;
+        } else {
+            preference.setSummary(getFullKernelVersion());
+            fullKernelVersion = true;
+        }
         return false;
     }
 
