3fef562cf82d62f505926fba9cbaf9b1778d6a1e
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2025-12-05 23:02:17 +0700

    Eunoia: Import Navigation Bar features
    Change-Id: I6fb604961e1d8497c738ba40ba1894e54b046859

---
+++ src/com/eunoiaos/settings/search/Searchable.java
@@ -0,0 +1,30 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The EunoiaOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.eunoiaos.settings.search;
+
+import android.content.Context;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * This interface should be implemented by classes which want to provide additional
+ * dynamic metadata to the indexer. Since our entrypoints are standardized around
+ * the parts catalog, there is no need to enumerate XML resources here. Keywords
+ * and non-indexable keys may be supplied by a class.
+ * <p>
+ * If a class wants to use this functionality, it should contain a static field
+ * named SEARCH_INDEX_DATA_PROVIDER which contains an instance of SearchIndexProvider.
+ * This is similar to the mechanism used by the Settings app.
+ */
+public interface Searchable {
+
+    interface SearchIndexProvider {
+
+        List<SearchIndexableRaw> getRawDataToIndex(Context context);
+
+        Set<String> getNonIndexableKeys(Context context);
+    }
+}
