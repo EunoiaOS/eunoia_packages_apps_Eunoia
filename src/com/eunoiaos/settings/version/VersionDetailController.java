70305b6daf93e9eb8a85d741d0203ab4ac0e7f96
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2025-12-09 19:39:48 +0700

    fixup! Eunoia: Reformat version info

--- src/com/eunoiaos/settings/version/VersionDetailController.java
+++ src/com/eunoiaos/settings/version/VersionDetailController.java
@@ -17,62 +17,33 @@
 package com.eunoiaos.settings.version;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.os.SystemClock;
 import android.os.SystemProperties;
-import android.os.UserHandle;
-import android.os.UserManager;
 import android.text.TextUtils;
 import android.util.Log;
 
-import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 
 import com.android.settings.R;
-import com.android.settings.Utils;
 import com.android.settings.core.BasePreferenceController;
-import com.android.settingslib.RestrictedLockUtils;
-import com.android.settingslib.RestrictedLockUtilsInternal;
 
 public class VersionDetailController extends BasePreferenceController {
     private static final String TAG = "eunoiaVersionCtrl";
     private static final String KEY_EUNOIA_VERSION = "eunoia_version";
-    private static final int DELAY_TIMER_MILLIS = 500;
-    private static final int ACTIVITY_TRIGGER_COUNT = 3;
 
     private static final String KEY_EUNOIA_VERSION_PROP = "ro.eunoia.version";
     private static final String KEY_EUNOIA_CODENAME_PROP = "ro.eunoia.codename";
     private static final String KEY_EUNOIA_DEVICE_PROP = "ro.eunoia.device";
     private static final String KEY_EUNOIA_RELEASE_PROP = "ro.eunoia.releasetype";
 
-    private final UserManager mUserManager;
-    private final long[] mHits = new long[ACTIVITY_TRIGGER_COUNT];
-
-    private RestrictedLockUtils.EnforcedAdmin mFunDisallowedAdmin;
-    private boolean mFunDisallowedBySystem;
-
     private boolean showCodename = false;
 
     public VersionDetailController(Context context, String key) {
         super(context, key);
-        mUserManager = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
-        initializeAdminPermissions();
     }
 
     @Override
     public int getAvailabilityStatus() {
         return AVAILABLE;
-    }
-
-    @Override
-    public boolean useDynamicSliceSummary() {
-        return true;
-    }
-
-    @Override
-    public boolean isSliceable() {
-        return true;
     }
 
     @Override
@@ -112,22 +83,6 @@ public class VersionDetailController extends BasePreferenceController {
             showCodename = true;
         }
         return false;
-    }
-
-    /**
-     * Copies the array onto itself to remove the oldest hit.
-     */
-    @VisibleForTesting
-    void arrayCopy() {
-        System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
-    }
-
-    @VisibleForTesting
-    void initializeAdminPermissions() {
-        mFunDisallowedAdmin = RestrictedLockUtilsInternal.checkIfRestrictionEnforced(
-                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
-        mFunDisallowedBySystem = RestrictedLockUtilsInternal.hasBaseUserRestriction(
-                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
     }
 
     private static String capitalize(String text) {
