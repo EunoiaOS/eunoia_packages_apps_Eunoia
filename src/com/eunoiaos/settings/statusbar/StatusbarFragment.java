1db5056dbf7c44993c00753acaf548c4195c9f7d
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2026-02-01 06:02:41 +0700

    Eunoia: Move battery customization into Eunoia [1/2]
    Change-Id: Idef6b290a7a28cd3abd9f01ffd70f51d12d8e985

--- src/com/eunoiaos/settings/statusbar/StatusbarFragment.java
+++ src/com/eunoiaos/settings/statusbar/StatusbarFragment.java
@@ -15,22 +15,90 @@
  */
 package com.eunoiaos.settings.statusbar;
 
+import android.content.Intent;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.fuelgauge.BatteryUtils;
 
-public class StatusbarFragment extends SettingsPreferenceFragment {
+import lineageos.preference.LineageSystemSettingListPreference;
+
+public class StatusbarFragment extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String CATEGORY_BATTERY = "status_bar_battery_key";
+    private static final String ICON_BLACKLIST = "icon_blacklist";
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
+    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+    private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
+
+    private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
+
+    private LineageSystemSettingListPreference mStatusBarBatteryShowPercent;
+
+    private PreferenceCategory mStatusBarBatteryCategory;
+
+    private boolean mBatteryPresent;
+
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
-
         addPreferencesFromResource(R.xml.top_level_statusbar_settings);
+
+        mStatusBarBatteryShowPercent = findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+        LineageSystemSettingListPreference statusBarBattery = findPreference(STATUS_BAR_BATTERY_STYLE);
+        statusBarBattery.setOnPreferenceChangeListener(this);
+        enableStatusBarBatteryDependents(statusBarBattery.getIntValue(2));
+
+        Intent intent = BatteryUtils.getBatteryIntent(getContext());
+        if (intent != null) {
+            mBatteryPresent = intent.getBooleanExtra(BatteryManager.EXTRA_PRESENT, true);
+        }
+        mStatusBarBatteryCategory = getPreferenceScreen().findPreference(CATEGORY_BATTERY);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        int value = Integer.parseInt((String) newValue);
+        String key = preference.getKey();
+        switch (key) {
+            case STATUS_BAR_BATTERY_STYLE:
+                enableStatusBarBatteryDependents(value);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        final String curIconBlacklist = Settings.Secure.getString(getContext().getContentResolver(), ICON_BLACKLIST);
+
+        if (!mBatteryPresent ||
+                TextUtils.delimitedStringContains(curIconBlacklist, ',', "battery")) {
+            getPreferenceScreen().removePreference(mStatusBarBatteryCategory);
+        } else {
+            getPreferenceScreen().addPreference(mStatusBarBatteryCategory);
+        }
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.EUNOIA_SETTINGS;
+    }
+
+    private void enableStatusBarBatteryDependents(int batteryIconStyle) {
+        mStatusBarBatteryShowPercent.setEnabled(batteryIconStyle != STATUS_BAR_BATTERY_STYLE_TEXT);
     }
 }
