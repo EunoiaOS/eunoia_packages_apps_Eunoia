6db9d57d3389297cce56cf17c06b3750a366aebb
eunoiaosofficial <eunoiaos.official@gmail.com>
2026-04-15 13:07:20 +0700

    Settings: Fix invalid SearchIndexable import
    Replace deprecated SearchIndexable import from settingslib
    with the correct settings package.
    
    This resolves build error caused by missing annotation class.
    
    Change-Id: I5543d453105e6f5d79f176de4ed8277aac55e7b3

--- src/com/eunoiaos/settings/eunoia/EunoiaTeam.java
+++ src/com/eunoiaos/settings/eunoia/EunoiaTeam.java
@@ -31,6 +31,7 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settingslib.search.SearchIndexable;
 import com.android.settingslib.drawer.Tile;
 
 @SearchIndexable
